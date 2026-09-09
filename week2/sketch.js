f7c9ce203a8f5f6668d0d1daed323074e7dbde03
geannagwinn <you@example.com>
2026-09-09 12:38:20 -0400

    changes

--- week2/sketch.js
+++ week2/sketch.js
@@ -4,7 +4,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   
 }
-
+//create canvas sets up the space to draw on screen and windowWidth and windowHeight are the dimensions of it
 function draw() {
   
 }
