3f1932ba7860e6f3778d01ded2ae71257524303d
geannagwinn <you@example.com>
2026-09-08 23:00:39 -0400

    background color

--- week1/sketch.js
+++ week1/sketch.js
@@ -33,7 +33,7 @@ function pickQuote() {
 }
 
 function draw() {
-  background(255, 20, 250); // set the background color
+  background(180, 250, 250); // set the background color
   drawQuote();  // draw the quote on screen
 }
 
