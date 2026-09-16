57ec3558c0884cbfbc3ef399a2aa2b6f48ea15eb
geannagwinn <you@example.com>
2026-09-15 23:38:55 -0400

    boba balls swallowed

--- week2/sketch.js
+++ week2/sketch.js
@@ -69,3 +69,88 @@ function setup() {
 //whitespace not important in javascript
 //origin is 0,0 coordinates are x,y
 //center point is width/2 height/2
+
+let xPos;
+let yPos;
+let y = 100;
+let x = 100;
+let randomx;
+windowWidth = 800;
+windowHeight = 800;
+
+function setup() {
+  //createCanvas(800, 800); //width, height
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB); //hue, saturation, brightness it goes over the color wheel ROYGBIV
+  background(177,100,100);
+  //what it's like to swallow a boba ball
+ // xPos = width/2; //whatever follows is a variable
+ // yPos = height/2; //whatever follows is a variable
+  //randomX = random(0,width/2);
+  noFill();
+  stroke('red');
+  ellipse(200,250,200, 350, 200);
+  ellipse(600,250,400, 700, 200);
+  ellipse(410,600,400, 300, 200);
+  stroke('black');
+
+}
+// scene 1: swallowing boba balls
+function draw(){
+   // ellipse(400,400,230,400);
+   // ellipse(xPos, yPos, 100);
+   // ellipse(xPos+randomX, yPos-30, 100);
+   // fill('brown');
+   ellipse(x, y, 100);
+   fill('brown');
+   x = x + 2; //makes balls move right (slower pace across screen)
+   y = y + 1; //makes balls move down only slightly 1 height
+
+   //going to draw what boba ball looks like swallowed in stomach as a concept?
+
+   if(mouseIsPressed){
+    y = random(0,height);
+    x = random(0,width);
+    fill ('green');// the x and y move to different spots but at the same iteration of 2 for x and 1 for y 
+}
+}
+
+//i drew three xray circles in the x ray of boba being stuck in someone's digestive system
+
+
+//i want to draw the chart i saw online of the boba balls in someone's digestive system
+  /* ellipse(100, 100, 100);
+   noFill(); //makes it look like slingies its cool tho idk 
+   */
+
+  
+
+
+
+//scene two: boba balls going down a slide
+
+/*function draw() {
+  //rect(400,400,400,400); //x,y,width,height
+  //ellipse(600,600,230,400); //x,y,radius, y height command ellipse does shape
+  //ellipse(400,400,230,400); //x,y,radius, y height command ellipse does shape
+  // tapioca ball that looks pretty with the background color
+  ellipse(400,400,230,400);
+  fill('brown');
+}
+  */
+
+    width = windowWidth;
+    height = windowHeight;
+
+  
+  /*
+  function mousePressed() {
+    xPos = mouseX; //whatever follows is a variable
+    yPos = mouseY; //whatever follows is a variable
+  
+    console.log("xPos: " + xPos + " yPos: " + yPos); //populates screen with circles
+    xPos = random(0,width); 
+    yPos = random(0,height); //places them random spots of screen with each click
+
+  }
+*/
