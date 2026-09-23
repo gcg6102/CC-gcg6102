ae590ab519ec3881f107bbdd0490f44503600620
geannagwinn <you@example.com>
2026-09-23 07:34:40 -0400

    comments

--- week3/sketch.js
+++ week3/sketch.js
@@ -166,6 +166,9 @@ function draw(){
 //pratice
   */
 
+
+//Frutiger Drawing One
+
 function setup(){
   createCanvas(800,600);
   colorMode(HSB);
@@ -175,11 +178,11 @@ function draw() {
   stroke(255, 0, 0);
   strokeWeight(5);
 
-  for(i = 0; i < 10; i++){
+  for(i = 0; i < 10; i++){ //I manipulated the loop for the line to see if I could make it curve, I also wanted to recreate the size changing circles (It is angled liens more than it is curved however)
     //line(i+400,i*10,50,i*5);
-    for(s = 0; s < 10; s++){
+    for(s = 0; s < 10; s++){ //Nested loop to change the components of the ellipse and the line and coloring
     ellipse(s*85, i*55, i * 4, i * 5);
-    fill('purple');
+    fill('purple'); 
     //rotate(65);
   }
     line(i-100,i*100,800,i+10);
